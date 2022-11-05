4c842d41eafbacaf3a2db66c9c1809c1f2cefb4f
filustre23 <fidel.ilustre@yahoo.com>
2022-11-05 18:00:31 -0400

    Create cartesian_product.sql

---
+++ cartesian_product.sql
@@ -0,0 +1,10 @@
+ -- code to create a row for each day, start time to end termination date 
+
+select 
+a.date::date as reporting_date,
+date_diff('d',b.date_hired,a.date) as days_since_hired,
+case when b.date_started is not null and a.date::date >= b.date_started::date then 1 else 0 end as active_flag,
+case when b.date_termed is not null and a.date::date >= b.date_termed::date then 1 else 0 end as inactive_flag
+from {{ source('schema', 'calendar') }} a, {{ source('schema', 'start_term_date_table')}} b
+where a.date::date >= b.date_hired::date and
+      a.date::date < getdate()::date
